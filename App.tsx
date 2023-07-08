9667039673f821208acc574925e9a6b3f9462838
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2023-07-08 13:26:56 +0530

    sample react native project

---
+++ App.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView>
+        <Text style={{fontSize: 30, backgroundColor: 'yellow'}} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ content.ts
@@ -0,0 +1,1 @@
+export const HEADER_TEXT = 'Sample react native app';
